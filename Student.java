ffaf9a8eedb8213633e8989de4c0833619886799
vidhibodhare21 <bodharevidhi@gmail.com>
2026-04-21 19:51:44 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,20 +1,8 @@
 
-public class EmployeeMain {
+public class HelloWorld {
 
 	public static void main(String[] args) {
-	     Employee e1 =  new Employee();
-	     e1.setEmpId(100);
-	     e1.setEmpName("Abc");
-	     e1.setEmpDept("IT");
-	     
-	     System.out.println(e1.getEmpId());
-	     System.out.println(e1.getEmpName());
-	     System.out.println(e1.getEmpDept());
-	     
-	     
-	     
-	     Employee e2 =  new Employee();
-	     
+		System.out.println(" Hello World "); 
 
 	}
 
